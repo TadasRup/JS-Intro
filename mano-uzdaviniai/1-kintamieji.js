aa3624fd47cdca8f1cda7229e9c783a022de50d5
Kevinas <129725556+TadasRup@users.noreply.github.com>
2023-05-11 09:00:50 +0300

    uzduotis

---
+++ mano-uzdaviniai/1-kintamieji.js
@@ -0,0 +1,56 @@
+console.clear();
+//Sukurti 3 kintamuosius su skaičiaus tipo reikšmėmis
+//Po kiekvieno jų inicijavimo, išvesti į console
+
+const number1 = 2;
+console.log(number1);
+
+const number2 = 4;
+console.log(number2);
+
+const number3 = 6;
+console.log(number3);
+
+console.clear();
+//Sukurti 3 kintamuosius su teksto tipo reikšmėmis
+//Po kiekvieno jų inicijavimo, išvesti į console
+
+const text1 = 'text';
+console.log(text1);
+
+const text2 = 'text';
+console.log(text2);
+
+const text3 = 'text';
+console.log(text3);
+
+console.clear();
+//Sukurti 3 sąrašo tipo kintamuosius su penkiomis skaičių tipo reikšmėmis
+//Po kiekvieno jų inicijavimo, išvesti į console
+
+const list1 = [1, 2, 3, 4, 5, ''];
+console.log(list1);
+
+const list2 = [2, 4, 6, 8, 10, ''];
+console.log(list2)
+
+const list3 = [ 4, 6, 8, 10, 12];
+console.log(list3);
+
+//Susumuoti visus skaičiaus tipo kintamuosius
+//Rezultatą išvesti į console
+const Sumsum = list1 + list2 + list3;
+console.log(Sumsum);
+
+
+//Sukurti 3 sąrašo tipo kintamuosius su penkiomis teksto tipo reikšmėmis
+//Po kiekvieno jų inicijavimo, išvesti į console
+
+const extext = ['pirmadienis', 'antradienis', 'treciadienis', 'ketvirtadienis', 'penktadienis'];
+console.log(extext);
+
+const extext2 = ['As', 'Tu', 'Jis', 'Ji', 'Jam'];
+console.log(extext2);
+
+const extext3 = ['Silta', 'Salta', 'Juoda', 'Balta', 'spalva']
+console.log(extext3)
